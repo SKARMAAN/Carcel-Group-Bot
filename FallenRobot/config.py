91485be722c772b164f355555e0e8b3eb5920626
SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>
2023-12-28 16:01:28 +0530

    Update config.py
    Signed-off-by: SKARMAAN <83514208+SKARMAAN@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -26,8 +26,8 @@ class Config(object):
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
+    DRAGONS = [6260714402, 5191699870, 1440712150]  # User id of sudo users
+    DEV_USERS = [5332414680]  # User id of dev users
     DEMONS = []  # User id of support users
     TIGERS = []  # User id of tiger users
     WOLVES = []  # User id of whitelist users
